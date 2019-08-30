d06a3619391147a32a0ed45282591400ae9a9a75
Vinicius Duarte <viniciusbatista023@gmail.com>
2019-08-30 07:52:04 -0300

    ajustando validacao para o metodo assertEquals e renomendo funcao de teste com nome descritivo

--- tests/Service/AvaliadorTest.php
+++ tests/Service/AvaliadorTest.php
@@ -11,7 +11,7 @@ use Alura\Leilao\Service\Avaliador;
 
 class AvaliadorTest extends TestCase
 {
-    public function testeUm()
+    public function testeAvaliadorDeveEncontrarOMaiorValorDeLanceEmOrdemCrescente()
     {
 
         /**
@@ -40,12 +40,38 @@ class AvaliadorTest extends TestCase
          * ASSERT - THEN
          * Validando retorno dos metodos (verificação do resultado)
          */
-        $valorEsperado = 2500;
+        self::assertEquals(2500, $maiorValor);
+    }
 
-        if ($maiorValor == $valorEsperado) {
-            echo 'TESTE OK';
-        } else {
-            echo 'TESTE FALHOU';
-        }
+    public function testeAvaliadorDeveEncontrarOMaiorValorDeLanceEmOrdemDecrescente()
+    {
+
+        /**
+         * ARRANGE - GIVEN
+         * Criando cenario para teste (inicialização do cenário)
+         */
+        $leilao = new Leilao('Fiat 147 0KM');
+
+        $maria = new Usuario('Maria ');
+        $joao = new Usuario('João');
+
+        $leilao->recebeLance(new Lance($maria, 2500));
+        $leilao->recebeLance(new Lance($joao, 2000));
+
+        $leiloeiro = new Avaliador();
+
+        /**
+         * ACT - WHEN
+         * Executando chamada das funcionalidades desenvolvidas (execução da regra de negócio)
+         */
+        $leiloeiro->avalia($leilao);
+
+        $maiorValor = $leiloeiro->getMaiorValor();
+
+        /**
+         * ASSERT - THEN
+         * Validando retorno dos metodos (verificação do resultado)
+         */
+        self::assertEquals(2500, $maiorValor);
     }
 }
