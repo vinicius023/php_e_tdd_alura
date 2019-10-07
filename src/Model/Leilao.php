154a60f3345c7dfff13de8dc3a1e45c11c7d1e8f
Vinicius Batista <viniciusbatista023@gmail.com>
2019-10-07 08:25:08 -0300

    Verificando lances de um mesmo usuario

--- src/Model/Leilao.php
+++ src/Model/Leilao.php
@@ -17,7 +17,16 @@ class Leilao
 
     public function recebeLance(Lance $lance)
     {
+        if (!empty($this->lances) && $this->ehDoUltimoUsuario($lance)) {
+            return;
+        }
         $this->lances[] = $lance;
+    }
+
+    public function ehDoUltimoUsuario(Lance $lance)
+    {
+        $ultimoLance = $this->lances[count($this->lances) - 1];
+        return $lance->getUsuario() == $ultimoLance->getUsuario();
     }
 
     /**
