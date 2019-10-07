8eb44395b20e2e72d6ff5ea07ceed933e52fabcf
Vinicius Batista <viniciusbatista023@gmail.com>
2019-10-07 08:06:31 -0300

    Adicionando testLeilaoNaoDeveReceberLancesRepetidos

--- tests/Model/LeilaoTest.php
+++ tests/Model/LeilaoTest.php
@@ -37,4 +37,16 @@ class LeilaoTest extends TestCase
             static::assertEquals($valor, $leilao->getLances()[$index]->getValor());
         }
     }
+
+    public function testLeilaoNaoDeveReceberLancesRepetidos()
+    {
+        $leilao = new Leilao('Variante 1994 99999KM');
+        $ana = new Usuario('Ana');
+
+        $leilao->recebeLance(new Lance($ana, 1000));
+        $leilao->recebeLance(new Lance($ana, 1500));
+
+        static::assertCount(1, $leilao->getLances());
+        static::assertEquals(1000, $leilao->getLances()[0]->getValor());
+    }
 }
